cf3d7a38fa1d2abef4ce982c07208ae3c670fbbc
mewarren <holla@mewarren.com>
2019-07-25 16:23:55 -0700

    added header

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,17 +1,18 @@
 import Template from '../Template';
 import html from './app.html';
 import Subjects from './subjects/Subjects';
+import Header from './header/Header';
 
 const template = new Template(html);
-
 const subjects = new Subjects(html);
+const header = new Header(html);
 
 export default class App{
   
   render(){
     const dom = template.clone();
-    const main = dom.getElementById('main-content');
-    main.appendChild(subjects.render());
+    dom.getElementById('main-head').appendChild(header.render());
+    dom.getElementById('main-content').appendChild(subjects.render());
     
     return dom;
   }
