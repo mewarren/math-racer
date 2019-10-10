311d90562912375b19eb680248fc77e8efe68708
mewarren <holla@mewarren.com>
2019-10-10 15:16:32 -0700

    add players css

--- src/components/players/Players.js
+++ src/components/players/Players.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import './players.css';
 
 class Players extends React.Component {
     render() {
         return(
             <div>
                 <p>Select the number of players</p>
+                <div className="selectPlayers">
+                    <p>1</p>
+                    <p>2</p>
+                    <p>3</p>
+                    <p>4</p>
+                </div>
 
             </div>
         );
