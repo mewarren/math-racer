013fca91d6a09a0f7956dd3feb5b69abc0f510c3
mewarren <holla@mewarren.com>
2019-10-10 14:29:14 -0700

    customizing template

--- src/App.js
+++ src/App.js
@@ -6,10 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Math Racer
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
