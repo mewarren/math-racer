59e35fa84e533089a0da345103362c2cf0ab5ae8
mewarren <holla@mewarren.com>
2019-10-10 13:40:19 -0700

    starting over with create react app

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import './reset.css';
-import './main.css';
-import App from './components/app/App';
-
-
-const root = document.getElementById('root');
-const app = new App();
-root.appendChild(app.render());
