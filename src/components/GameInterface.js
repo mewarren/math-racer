7ab79dbe490f1fc955fffcf4dffcfa80419f2a8c
mewarren <holla@mewarren.com>
2019-11-09 10:07:53 -0800

    save

--- src/components/GameInterface.js
+++ src/components/GameInterface.js
@@ -4,21 +4,46 @@ import Question from './Question';
 export default class GameInterface extends React.Component {
     constructor(props) {
         super(props);
-        this.handleClick = this.handleClick.bind(this);
+        // this.handleClick = this.handleClick.bind(this);
+        this.setName = this.setName.bind(this);
+
+        this.state = { 
+            playerReady : false,
+            playerName : null
+        };
 
     }
 
-    handleClick() {
-        this.props.nextPlayer();
+    setName(){
+        let { playerInfo, activePlayer } = this.props;
+        let player = playerInfo[activePlayer];
+        console.log(playerInfo);
+        this.setState({
+            playerName : 'player name'
+        });
     }
+
+    componentWillMount(){
+        console.log('GI is mounting');
+
+        this.setName();
+    }
+    // handleClick() {
+    //     this.props.nextPlayer();
+    // }
 
     render() {
-        return((this.props.gameStatus) ?
+        const { gameStatus, activePlayer } = this.props;
+        return((activePlayer > 0) ?
+            // <div>
+            //     <Question subject={this.props.subject}/>
+            //     {/* <button onClick={this.handleClick}>Enter</button> */}
+            // </div>
+            // : 
             <div>
-                <Question subject={this.props.subject}/>
-                <button onClick={this.handleClick}>Enter</button>
-            </div>
-            : null
+                <p> {this.state.playerName} are you ready</p>
+                <button>Let's Go!</button>
+            </div>: null
         );
     }
 }
