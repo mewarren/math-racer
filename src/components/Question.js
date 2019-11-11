76972515164c4bec598f5e39c4d3bb275d49e5f8
mewarren <holla@mewarren.com>
2019-11-11 12:15:40 -0800

    add checkAnswer to Question component

--- src/components/Question.js
+++ src/components/Question.js
@@ -4,19 +4,21 @@ export default class Question extends React.Component {
     constructor(props) {
         super(props);
         this.generateQuestion = this.generateQuestion.bind(this);
+        this.checkAnswer = this.checkAnswer.bind(this);
 
         this.state = {
             subject : this.props.subject,
             problem : null,
-            answer : null
+            answer : null,
+            attempts : 0
         };
     }
 
-    generateQuestion = (subject) => {
+    generateQuestion() {
         let getRandomInt = max => 
-        Math.floor(Math.random() * Math.floor(max));
+            Math.floor(Math.random() * Math.floor(max));
 
-        let operator = (subject === 'add') ? " + " : " - "; 
+        const operator = (this.state.subject === 'add') ? " + " : " - "; 
         let problem = getRandomInt(9) + operator + getRandomInt(9);
 
         this.setState({
@@ -29,11 +31,24 @@ export default class Question extends React.Component {
         this.generateQuestion(this.state.problem);
      }
 
+     checkAnswer(){
+        let answer = document.getElementById('answer');
+        console.log(answer.value);
+        (answer.value === this.state.answer) ? 
+           console.log("Correct!") : console.log("incorrect!");
+        answer.value = null;
+        this.setState(prevState => {
+            return {attempts : prevState.attempts + 1}
+        });
+     }
+
     render() {
         return(
             <div>
                  <p>What is the answer to</p>
                  <h4>{this.state.problem}</h4>
+                 <input type="text" name="answer" id="answer"></input>
+                <button onClick={this.checkAnswer}>Check</button>
             </div>
         );
     }
