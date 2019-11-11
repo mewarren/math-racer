fabf5bcec45608e8f57ab6b9f9a683d8c828bc18
mewarren <holla@mewarren.com>
2019-11-11 00:25:47 -0800

    fix player ready in GameInterface

--- src/components/GameInterface.js
+++ src/components/GameInterface.js
@@ -1,48 +1,37 @@
 import React from 'react';
-import Question from './Question';
+// import Question from './Question';
 
 export default class GameInterface extends React.Component {
     constructor(props) {
         super(props);
-        // this.handleClick = this.handleClick.bind(this);
-        this.setName = this.setName.bind(this);
+        this.handleClick = this.handleClick.bind(this);
 
         this.state = { 
             playerReady : false,
-            playerName : null
         };
-
     }
 
-    setName(){
-        let { playerInfo, activePlayer } = this.props;
-        let player = playerInfo[activePlayer];
-        console.log(playerInfo);
+    handleClick() {
         this.setState({
-            playerName : 'player name'
+            playerReady : true
         });
+        // this.props.nextPlayer();
     }
-
-    componentWillMount(){
-        console.log('GI is mounting');
-
-        this.setName();
-    }
-    // handleClick() {
-    //     this.props.nextPlayer();
-    // }
 
     render() {
-        const { gameStatus, activePlayer } = this.props;
-        return((activePlayer > 0) ?
+        const { gameStatus, player } = this.props;
+        return((gameStatus !== false) ?
             // <div>
             //     <Question subject={this.props.subject}/>
             //     {/* <button onClick={this.handleClick}>Enter</button> */}
             // </div>
             // : 
             <div>
-                <p> {this.state.playerName} are you ready</p>
-                <button>Let's Go!</button>
+                <p> {player.name} are you ready</p>
+                
+                {(this.state.playerReady===false) ? 
+                <button onClick={this.handleClick}>Let's Go!</button> 
+                : null}
             </div>: null
         );
     }
